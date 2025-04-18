3b4dbe98ff56ffd453933ebe097505a8b68197bc
Mohamed Aadhil <mohamed_aadhil@outlook.com>
2025-04-18 12:29:04 +0530

    Initial Commit - Developed WebSocket API Endpoint for continuous streaming.

---
+++ Controllers/WebSocketController.cs
@@ -0,0 +1,46 @@
+﻿using ForeignTimeWebSocket.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ForeignTimeWebSocket.Controllers;
+
+[ApiController]
+public class WebSocketController : ControllerBase
+{
+    private readonly TimeService _timeService;
+
+    public WebSocketController(TimeService timeService)
+    {
+        _timeService = timeService;
+    }
+
+    [Route("/ws/time")]
+    public async Task Get()
+    {
+        if (HttpContext.WebSockets.IsWebSocketRequest)
+        {
+            var webSocket = await HttpContext.WebSockets.AcceptWebSocketAsync();
+            var buffer = new byte[1024 * 4];
+
+            var result = await webSocket.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+            var timeZoneId = System.Text.Encoding.UTF8.GetString(buffer, 0, result.Count);
+
+            Console.WriteLine($"[WebSocket] Time zone selected: {timeZoneId}");
+
+            while (webSocket.State == System.Net.WebSockets.WebSocketState.Open)
+            {
+                var time = _timeService.GetCurrentTimeForZone(timeZoneId);
+                var json = System.Text.Json.JsonSerializer.Serialize(time);
+                var bytes = System.Text.Encoding.UTF8.GetBytes(json);
+                await webSocket.SendAsync(new ArraySegment<byte>(bytes), System.Net.WebSockets.WebSocketMessageType.Text, true, CancellationToken.None);
+
+                await Task.Delay(1000);
+            }
+
+            await webSocket.CloseAsync(System.Net.WebSockets.WebSocketCloseStatus.NormalClosure, "Done", CancellationToken.None);
+        }
+        else
+        {
+            HttpContext.Response.StatusCode = 400;
+        }
+    }
+}
---
+++ Services/TimeService.cs
@@ -0,0 +1,28 @@
+﻿using ForeignTimeWebSocket.Models;
+
+namespace ForeignTimeWebSocket.Services;
+
+public class TimeService
+{
+    public TimeResponse GetCurrentTimeForZone(string timeZoneId)
+    {
+        try
+        {
+            var timeZoneInfo = TimeZoneInfo.FindSystemTimeZoneById(timeZoneId);
+            var localTime = TimeZoneInfo.ConvertTime(DateTime.UtcNow, timeZoneInfo);
+            return new TimeResponse
+            {
+                Time = localTime.ToString("HH:mm:ss"),
+                TimeZone = timeZoneId
+            };
+        }
+        catch
+        {
+            return new TimeResponse
+            {
+                Time = "Invalid Time Zone",
+                TimeZone = timeZoneId
+            };
+        }
+    }
+}
